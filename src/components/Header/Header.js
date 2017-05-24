ae49904eaca3f4d083f7fbc63bc725c785d85ed3
Miguel <miguel.solorio07@gmail.com>
2017-05-24 10:39:14 -0700

    Switch Routes to use HashRouter instead of BrowserRoute for gh-pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 import NavLink from '../../modules/NavLink'
 import Characters from '../../components/Pages/Characters'

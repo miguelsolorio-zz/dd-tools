472841abffce5466bd644c731b2aa9cf050ace85
Miguel <miguel.solorio07@gmail.com>
2017-10-05 10:42:18 -0700

    Minor changes

--- src/components/Pages/Characters.js
+++ src/components/Pages/Characters.js
@@ -5,7 +5,7 @@ export default class Characters extends React.Component {
     return (
       <div>
         <h1>Character List</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu pretium ligula, porta scelerisque ex. Sed aliquam laoreet nisl, in ultricies libero vulputate ut. Pellentesque a erat libero. Integer a ante faucibus ante mollis consectetur. Ut vitae ex ipsum. Donec dapibus est eu magna tincidunt, et efficitur lacus finibus. Mauris non massa dui. Etiam eros dui, condimentum eget euismod a, sodales sed velit. Pellentesque ac rhoncus est. Donec eleifend ipsum nunc, vitae lobortis tellus consequat id. Nulla pharetra vitae turpis quis condimentum. In et lorem vitae libero tincidunt tincidunt vel sollicitudin lacus. Curabitur eu dignissim mauris. Morbi suscipit finibus lectus, ut tempus lectus viverra ut.</p>
+        <p>Sample list will be here</p>
       </div>
     );
   }
--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -6,7 +6,6 @@ export default class Contact extends React.Component {
       <div>
         <h1>Contact</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu pretium ligula, porta scelerisque ex. Sed aliquam laoreet nisl, in ultricies libero vulputate ut. Pellentesque a erat libero. Integer a ante faucibus ante mollis consectetur. Ut vitae ex ipsum. Donec dapibus est eu magna tincidunt, et efficitur lacus finibus. Mauris non massa dui. Etiam eros dui, condimentum eget euismod a, sodales sed velit. Pellentesque ac rhoncus est. Donec eleifend ipsum nunc, vitae lobortis tellus consequat id. Nulla pharetra vitae turpis quis condimentum. In et lorem vitae libero tincidunt tincidunt vel sollicitudin lacus. Curabitur eu dignissim mauris. Morbi suscipit finibus lectus, ut tempus lectus viverra ut.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu pretium ligula, porta scelerisque ex. Sed aliquam laoreet nisl, in ultricies libero vulputate ut. Pellentesque a erat libero. Integer a ante faucibus ante mollis consectetur. Ut vitae ex ipsum. Donec dapibus est eu magna tincidunt, et efficitur lacus finibus. Mauris non massa dui. Etiam eros dui, condimentum eget euismod a, sodales sed velit. Pellentesque ac rhoncus est. Donec eleifend ipsum nunc, vitae lobortis tellus consequat id. Nulla pharetra vitae turpis quis condimentum. In et lorem vitae libero tincidunt tincidunt vel sollicitudin lacus. Curabitur eu dignissim mauris. Morbi suscipit finibus lectus, ut tempus lectus viverra ut.</p>
       </div>
     );
   }
